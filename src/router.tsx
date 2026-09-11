b6889b5f83e282875a593f13104e999fef4bfd74
Chris Birster <15853349+chrisbirster@users.noreply.github.com>
2026-09-11 09:10:31 -0400

    M1: route create and settings to creator workspace

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,19 @@
 import { createRouter } from "@solidjs/router";
 import {
-  CreatePage,
   DiscoverPage,
   HomePage,
   NotFoundPage,
   ProfilePage,
 } from "./pages";
+import { EditorPage, SettingsPage } from "./editor";
 import { SignInPage } from "./signin";
 
 export const Router = createRouter({
   routes: [
     { path: "/", component: HomePage },
     { path: "/discover", component: DiscoverPage },
-    { path: "/create", component: CreatePage },
+    { path: "/create", component: EditorPage },
+    { path: "/settings", component: SettingsPage },
     { path: "/signin", component: SignInPage },
     { path: "/:handle", component: ProfilePage },
     { path: "*404", component: NotFoundPage },
