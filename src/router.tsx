b213da6747064d7eb848990058698d602b386cee
Chris Birster <christopher.birster@gmail.com>
2026-09-13 00:03:10 -0400

    feat: route share and import tools

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import {
   ProfilePage,
 } from "./pages";
 import { EditorPage, SettingsPage } from "./editor";
+import { LinkToolsPage } from "./link-tools";
 import { SignInPage } from "./signin";
 
 export const Router = createRouter({
@@ -14,6 +15,7 @@ export const Router = createRouter({
     { path: "/discover", component: DiscoverPage },
     { path: "/create", component: EditorPage },
     { path: "/settings", component: SettingsPage },
+    { path: "/settings/import", component: LinkToolsPage },
     { path: "/signin", component: SignInPage },
     { path: "/:handle", component: ProfilePage },
     { path: "*404", component: NotFoundPage },
